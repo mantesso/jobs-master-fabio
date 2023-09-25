2695cc223ae891d3bb679c29d21d016e2ae0c2d6
mantesso <fmantesso@gmail.com>
2023-09-25 17:24:14 +0200

    add character stats model methods

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,6 @@
 # frozen_string_literal: true
 class PagesController < ApplicationController
+  def index
+    @characters = Character.all
+  end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -7,4 +7,20 @@ class Character < ApplicationRecord
                           numericality: { only_integer: true, greater_than_or_equal_to: 50, less_than_or_equal_to: 200 }
   validates :attack_points, presence: true,
                             numericality: { only_integer: true, greater_than_or_equal_to: 5, less_than_or_equal_to: 50 }
+
+  def fights
+    Fight.where('character1_id = ? OR character2_id = ?', id, id).count
+  end
+
+  def wins
+    Fight.where(winner_id: id).count
+  end
+
+  def win_rate
+    total_fights = fights
+    return 0 if total_fights.zero?
+
+    total_wins = wins
+    (total_wins.to_f / total_fights * 100).round(2)
+  end
 end
